f7ea6f2563641bd45612e9d43b2d714a1ac4e885
Dmitry Lorents <zmey.task@gmail.com>
2023-12-26 23:06:58 +0300

    Add extensions', cell's and image files

---
+++ MovieHub/Screens/Wishlist/View/Cells/WishListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WishListTableViewCell.swift
+//  MovieHub
+//
+//  Created by Dmitry on 26.12.2023.
+//
+
+import UIKit
+
+class WishListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
