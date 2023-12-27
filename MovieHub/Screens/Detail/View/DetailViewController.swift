29121804753d9c0e4a7f8ec064e3b5488f80b78d
Igor Guryan <sacujo93@yandex.ru>
2023-12-27 17:41:02 +0300

    edit model

--- MovieHub/Screens/Detail/View/DetailViewController.swift
+++ MovieHub/Screens/Detail/View/DetailViewController.swift
@@ -11,13 +11,25 @@ final class DetailViewController: UIViewController {
     
     //MARK: Properties
     var presenter: DetailPresenterProtocol?
+    var networkManager = NetworkkService()
     
     //MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        fetch("5304403")
         view.backgroundColor = .purple
         
+    }
+    
+    func fetch(_ id: String) {
+        networkManager.searchID(id) {(result: Result<DetailModel, RequestError>) in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async { print(data) }
+            case .failure(let error):
+                print(error.customMessage)
+            }
+        }
     }
 }
 
