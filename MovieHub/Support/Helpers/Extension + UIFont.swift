566b871ec67cb2d744c1659bc74992e447a76882
Evgenii Sidorov <sidzheone@gmail.com>
2023-12-27 12:26:46 +0300

    Added color and fonts

---
+++ MovieHub/Support/Helpers/Extension + UIFont.swift
@@ -0,0 +1,46 @@
+//
+//  Extension + UIFont.swift
+//  MovieHub
+//
+//  Created by sidzhe on 27.12.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    static func montserratBlack(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Black", size: size)
+    }
+    
+    static func montserratBold(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Bold", size: size)
+    }
+    
+    static func montserratExtraBold(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-ExtraBold", size: size)
+    }
+    
+    static func montserratExtraLight(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-ExtraLight", size: size)
+    }
+    
+    static func montserratLight(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Light", size: size)
+    }
+    
+    static func montserratMedium(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Medium", size: size)
+    }
+    
+    static func montserratRegular(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Regular", size: size)
+    }
+    
+    static func montserratSemiBold(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-SemiBold", size: size)
+    }
+    
+    static func montserratThin(size: CGFloat) -> UIFont? {
+        UIFont(name: "Montserrat-Thin", size: size)
+    }
+}
