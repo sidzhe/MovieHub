c6c61564b3f7ee50c066c01470d1a708fe0bf795
DmKeller <dmit.keller@gmail.com>
2023-12-28 17:58:54 +0300

    fix colllection

--- MovieHub/Screens/Search/View/SearchViewController.swift
+++ MovieHub/Screens/Search/View/SearchViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    enum Section {
+        case category
+        case upcomingMovie
+        case recentMovie
+    }
+    
     //MARK: Properties
     var presenter: SearchPresenterProtocol?
     
@@ -49,17 +55,19 @@ final class SearchViewController: UIViewController {
     // MARK: - Setup
     
     private func registerCollectionsCells() {
+       
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "YourCellReuseIdentifier")
         collectionView.register(RecentMovieCollectionViewCell.self, forCellWithReuseIdentifier: RecentMovieCollectionViewCell.identifier )
     }
     
     private func setupHierarchy() {
+        collectionView.backgroundColor = .black
         view.addSubview(collectionView)
     }
     private func setupLayout() {
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.topAnchor,constant: 186),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor,constant: 144),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
@@ -69,14 +77,16 @@ final class SearchViewController: UIViewController {
     // MARK: - CompositionalLayout
     private func createLayout() -> UICollectionViewCompositionalLayout {
         return UICollectionViewCompositionalLayout { sectionIndex, _ in
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2),
-                                                  heightDimension: .fractionalWidth(0.2))
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                                  heightDimension: .fractionalHeight(0.1))
             let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                   heightDimension: .fractionalHeight(1))
-            let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: layoutItem, count: 5)
-            let sectionLayout = NSCollectionLayoutSection(group: layoutGroup)
-            return sectionLayout
+            layoutItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9),
+                                                   heightDimension: .absolute(20))
+            let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [layoutItem])
+            let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+            layoutSection.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            return layoutSection
         }
     }
 }
