e558a4d3ddf1e10ecfd3b5fae30cff9ae6d1a529
Dima <dorogovd@yandex.ru>
2024-01-13 19:56:28 +0300

    transition style update

--- MovieHub/Screens/Onboarding/View/OnboardingViewController.swift
+++ MovieHub/Screens/Onboarding/View/OnboardingViewController.swift
@@ -156,14 +156,13 @@ extension OnboardingViewController {
 extension UIPageViewController {
     
     func goToNextPage(animated: Bool = true, completion: ((Bool) -> Void)? = nil) {
+        
         guard let currentPage = viewControllers?[0] else { return }
         guard let nextPage = dataSource?.pageViewController(self, viewControllerAfter: currentPage) else { return }
-        
         setViewControllers([nextPage], direction: .forward, animated: animated, completion: completion)
     }
 }
 
 //MARK: - Extension OnboardingViewProtocol
 extension OnboardingViewController: OnboardingViewProtocol {
-    
 }
